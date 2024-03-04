b8967091f2dcb5d6e9017d426a251f4596590987
Francisco Hernandez Valenzuela <franciscohv95@gmail.com>
2024-03-04 18:53:02 -0300

    add home's text & improve home's child components

--- src/components/carrusel/Carrusel.jsx
+++ src/components/carrusel/Carrusel.jsx
@@ -25,13 +25,13 @@ const Carrusel = ({ children: slides, autoSlide = false, autoSlideInterval = 400
                     <IoChevronForwardOutline size={40} />
                 </button>
             </div>
-            <div className="absolute bottom-4 right-0 left-0">
+            {/* <div className="absolute bottom-4 right-0 left-0">
                 <div className="flex items-center justify-center gap-2">
                     {slides.map((s, i) => (
                         <div key={i} className={`transition-all w-3 h-3 bg-white rounded-full ${current === i ? "p-2" : "bg-opacity-50"}`}></div>
                     ))}
                 </div>
-            </div>
+            </div> */}
         </div>
     )
 }
