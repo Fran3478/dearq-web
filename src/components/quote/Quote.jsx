c1be668aa8219228a215b819d086351c32befb48
Francisco Hernandez Valenzuela <franciscohv95@gmail.com>
2024-03-07 20:44:21 -0300

    improves home component

--- src/components/quote/Quote.jsx
+++ src/components/quote/Quote.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types"
 const Quote = ({ imgUrl, quote, name, degree }) => {
 
     return (
-        <div className="bg-slate-300 grid grid-cols-6 lg:grid-cols-12 w-full p-[1.5rem] lg:p-0 lg:rounded-l-full select-none mb-3 overflow-hidden">
+        <div className="bg-slate-200 grid grid-cols-6 lg:grid-cols-12 w-full p-[1.5rem] lg:p-0 lg:rounded-l-full select-none overflow-hidden">
             <div className="float-left sm:float-none w-fit h-fit rounded-xl m-4 sm:m-0 sm:rounded-full overflow-hidden col-span-2 lg:col-span-4 2xl:col-span-3 hidden lg:block">
                 <img className="w-40 h-40 xl:w-44 xl:h-44 object-cover pointer-events-none" src={imgUrl} alt="" />
             </div>
