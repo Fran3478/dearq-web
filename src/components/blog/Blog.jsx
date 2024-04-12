7f2cf6427afcc504456f33b3a2cccc874e526397
Francisco Hernandez Valenzuela <franciscohv95@gmail.com>
2024-04-12 11:26:33 -0300

    button modularizate and re size title img

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -8,14 +8,14 @@ const Blog = () => {
             cat: "Categoria",
             img: "https://adrianxaviermx.wordpress.com/wp-content/uploads/2017/09/pabellon-el-eco-2011-foto.jpg",
             img_alt: "titulo-alternativo-img",
-            img_title: "Titulo para mostrar en la imagen",
+            img_title: "Lorem ipsum dolor sit amet consectetur",
             intro: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente modi recusandae quae numquam voluptates itaque tempora, in soluta iusto et dolores assumenda consequatur expedita atque natus animi voluptatum reiciendis accusantium?"
         },
         {
             id: 2,
             title: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
             cat: "Categoria 2",
-            img: "https://adrianxaviermx.wordpress.com/wp-content/uploads/2017/09/pabellon-el-eco-2011-foto.jpg",
+            img: "https://3.bp.blogspot.com/-7OxCvwEOq0k/T_zta0uIz-I/AAAAAAAAAC0/DrTRfGbW3oE/s1600/blog-del-dise%25C3%25B1o-producto-colombia.jpg",
             img_alt: "titulo-alternativo-img",
             img_title: "Titulo para mostrar en la imagen",
             intro: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente modi recusandae quae numquam voluptates itaque tempora, in soluta iusto et dolores assumenda consequatur expedita atque natus animi voluptatum reiciendis accusantium?"
@@ -24,7 +24,7 @@ const Blog = () => {
             id: 3,
             title: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
             cat: "Categoria 3",
-            img: "https://adrianxaviermx.wordpress.com/wp-content/uploads/2017/09/pabellon-el-eco-2011-foto.jpg",
+            img: "https://www.connectionsbyfinsa.com/wp-content/uploads/2019/05/ConnectionsbyFinsa-Dise%C3%B1o-Emocional-nurture.jpg",
             img_alt: "titulo-alternativo-img",
             img_title: "Titulo para mostrar en la imagen",
             intro: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente modi recusandae quae numquam voluptates itaque tempora, in soluta iusto et dolores assumenda consequatur expedita atque natus animi voluptatum reiciendis accusantium?"
@@ -33,7 +33,7 @@ const Blog = () => {
             id: 4,
             title: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
             cat: "Categoria 4",
-            img: "https://adrianxaviermx.wordpress.com/wp-content/uploads/2017/09/pabellon-el-eco-2011-foto.jpg",
+            img: "https://adictec.com/wp-content/uploads/2017/03/Dise%C3%B1o-Emocional-Para-Incrementar-La-Participaci%C3%B3n-Del-Usuario.jpg",
             img_alt: "titulo-alternativo-img",
             img_title: "Titulo para mostrar en la imagen",
             intro: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente modi recusandae quae numquam voluptates itaque tempora, in soluta iusto et dolores assumenda consequatur expedita atque natus animi voluptatum reiciendis accusantium?"
