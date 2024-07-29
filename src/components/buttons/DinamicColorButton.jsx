704d5e665b9b91b9a6d4d9f8168ecb477a8d00b4
Francisco Hernandez <franciscohv95@gmail.com>
2024-07-29 19:47:02 -0300

    pretty button

--- src/components/buttons/DinamicColorButton.jsx
+++ src/components/buttons/DinamicColorButton.jsx
@@ -4,7 +4,17 @@ const DinamicColorButton = ({text, icon, handleClick, disabledButton = false, co
 
     return(
         <button 
-            className={`rounded-md ${disabledButton ? "bg-gray-300 text-gray-500 border-2 border-gray-500" : `bg-white hover:text-white xs:text-xl font-semibold w-fit py-[0.5rem] px-[2rem] transition-all hover:shadow-md ${color === "blue" ? "text-blue-500 border-2 border-blue-500 hover:bg-blue-500" : color === "green" ? "text-green-500 border-2 border-green-500 hover:bg-green-500" : color === "yellow" ? "text-yellow-500 border-2 border-yellow-500 hover:bg-yellow-500" : color === "red_ligth" ? "text-red-500 border-2 border-red-500 hover:bg-red-500" : color === "red_full" && "text-white hover:text-red-500 border-2 border-red-500 bg-red-500 hover:bg-white" } text-[#f2840a] border-2 border-[#f2840a] hover:bg-[#f2840a]"} ` }`}
+            className={
+                `min-w-[6rem] rounded-md ${disabledButton ? 
+                "bg-gray-300 text-gray-500 border-2 border-gray-500" : 
+                `font-semibold transition-all hover:shadow-md 
+                ${color === "blue" ? "text-blue-500 border-2 hover:text-white border-blue-500 hover:bg-blue-500" : 
+                color === "green" ? "text-green-500 hover:text-white border-2 border-green-500 hover:bg-green-500" : 
+                color === "yellow" ? "text-yellow-500 hover:text-white border-2 border-yellow-500 hover:bg-yellow-500" : 
+                color === "red_ligth" ? "text-red-500 hover:text-white border-2 border-red-500 hover:bg-red-500" : 
+                color === "red_full" ? "text-white hover:text-red-500 border-2 border-red-500 bg-red-500 hover:bg-white" : 
+                "text-[#f2840a] hover:text-white border-2 border-[#f2840a] hover:bg-[#f2840a]" } "} ` }`
+            }
             onClick={handleClick}
             disabled={disabledButton}
         >
@@ -25,7 +35,7 @@ DinamicColorButton.propTypes = {
     icon: PropTypes.element,
     handleClick: PropTypes.func,
     disabledButton: PropTypes.bool,
-    color: PropTypes.oneOf("blue", "red_light", "red_full", "green", "yellow")
+    color: PropTypes.oneOf(["blue", "red_light", "red_full", "green", "yellow"])
 }
 
 export default DinamicColorButton
