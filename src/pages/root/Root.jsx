f63efff8918ce3be9021b9336a79737654fab945
Francisco Hernandez <franciscohv95@gmail.com>
2024-08-02 01:20:42 -0300

    update public posts features

--- src/pages/root/Root.jsx
+++ src/pages/root/Root.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom"
 import Header from "../../components/header/Header"
 import Footer from "../../components/footer/Footer"
+import { PublicPostsProvider } from "../../context/publicPosts"
 
 const Root = () => {
 
@@ -8,7 +9,9 @@ const Root = () => {
         <div className="flex flex-col min-h-screen bg-gray-100">
             <Header/>
             <div className="flex-grow flex h-full">
-                <Outlet/>
+                <PublicPostsProvider>
+                    <Outlet/>
+                </PublicPostsProvider>
             </div>
             <Footer/>
         </div>
