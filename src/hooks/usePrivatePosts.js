236deccec280bed6c320ba77f81c36fbba1d4631
Francisco Hernandez <franciscohv95@gmail.com>
2024-09-16 18:59:21 -0300

    update routes

--- src/hooks/usePrivatePosts.js
+++ src/hooks/usePrivatePosts.js
@@ -1,36 +1,37 @@
-import { useContext, useState } from "react"
-import { PrivatePostsContext } from "../context/privatePosts"
-import axios from "axios"
+import { useContext, useState } from "react";
+import { PrivatePostsContext } from "../context/privatePosts";
+import axios from "axios";
 
 const usePrivatePosts = () => {
+  const { data, setData } = useContext(PrivatePostsContext);
 
-    const {data, setData} = useContext(PrivatePostsContext)
+  const [error, setError] = useState(null);
 
-    const [error, setError] = useState(null)
-
-    const getPosts = async () => {
-        try {
-            const response = await axios.get(`${import.meta.env.VITE_BASE_URL + import.meta.env.VITE_ADMIN_ALL_POSTS_URL}`)
-            setData({
-                posts: response.data.rows,
-                totalPosts: response.data.totalPosts,
-                currentPage: response.data.currentPage,
-                totalPages: response.data.totalPages
-            })
-        } catch (err) {
-             console.log(err)
-            setError(err)
-        }
+  const getPosts = async () => {
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_BASE_URL}${import.meta.env.VITE_PRIVATE_URL}${import.meta.env.VITE_POST_URL}`,
+      );
+      setData({
+        posts: response.data.rows,
+        totalPosts: response.data.totalPosts,
+        currentPage: response.data.currentPage,
+        totalPages: response.data.totalPages,
+      });
+    } catch (err) {
+      console.log(err);
+      setError(err);
     }
+  };
 
-    const updatePublishedPost = (id) => {
-        const updateList = data.posts.map(post => 
-            post.id === id ? {...post, published: !post.published} : post
-        )
-        setData({...data, posts: updateList})
-    }
+  const updatePublishedPost = (id) => {
+    const updateList = data.posts.map((post) =>
+      post.id === id ? { ...post, published: !post.published } : post,
+    );
+    setData({ ...data, posts: updateList });
+  };
 
-    return {data, getPosts, updatePublishedPost, error}
-}
+  return { data, getPosts, updatePublishedPost, error };
+};
 
-export default usePrivatePosts
+export default usePrivatePosts;
