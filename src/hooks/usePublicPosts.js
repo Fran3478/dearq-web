3ce13118f7bfc387b28a89c9da49a5e778ede725
Francisco Hernandez <franciscohv95@gmail.com>
2024-09-23 21:06:43 -0300

    impruve hook to return precise loading status

--- src/hooks/usePublicPosts.js
+++ src/hooks/usePublicPosts.js
@@ -1,31 +1,37 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { PublicPostsContext } from "../context/publicPosts";
 import axios from "axios";
 
 const usePublicPosts = () => {
   const { data, setData } = useContext(PublicPostsContext);
-
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  const getPosts = async () => {
-    try {
-      const response = await axios.get(
-        `${import.meta.env.VITE_BASE_URL}${import.meta.env.VITE_PUBLIC_URL}${import.meta.env.VITE_POST_URL}`,
-      );
-      console.log(response);
-      setData({
-        posts: response.data.rows,
-        totalPosts: response.data.totalPosts,
-        currentPage: response.data.currentPage,
-        totalPages: response.data.totalPages,
-      });
-    } catch (err) {
-      setError(err);
-      console.log(err);
-    }
-  };
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(
+          import.meta.env.VITE_BASE_URL +
+            import.meta.env.VITE_PUBLIC_URL +
+            import.meta.env.VITE_POST_URL,
+        );
+        setData({
+          posts: response.data.rows,
+          totalPosts: response.data.totalPosts,
+          currentPage: response.data.currentPage,
+          totalPages: response.data.totalPages,
+        });
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchPosts();
+  }, []);
 
-  return { data, getPosts, error };
+  return { data, loading, error };
 };
 
 export default usePublicPosts;
