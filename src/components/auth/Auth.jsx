9a80547ff49e28d2ef99162eea5fd890532cb877
Francisco Hernandez <franciscohv95@gmail.com>
2024-10-01 12:48:10 -0300

    update components to token handling

--- src/components/auth/Auth.jsx
+++ src/components/auth/Auth.jsx
@@ -1,24 +1,27 @@
-import { Outlet, useNavigate } from "react-router-dom"
-import { useAuth } from "../../hooks/useAuth"
-import { useEffect } from "react"
+import { Outlet, useNavigate } from "react-router-dom";
+import { useAuth } from "../../hooks/useAuth";
+import { useEffect } from "react";
 
 const Auth = () => {
-    const {isAuthenticated, isAdmin} = useAuth()
-    const navigate = useNavigate()
+  const { isAuthenticated, isAdmin, isTokenLoading } = useAuth();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        if(isAuthenticated()) {
-            if(isAdmin()) return navigate("/admin/posts", {replace: true})
-            return navigate("/home", {replace: true})
-        } else {
-            return navigate("/auth/login")
-        }
-    }, [])
-    return(
-        <>
-            <Outlet/>
-        </>
-    )
-}
+  useEffect(() => {
+    if (isTokenLoading) return;
 
-export default Auth
+    if (isAuthenticated()) {
+      if (isAdmin()) return navigate("/admin/posts", { replace: true });
+      return navigate("/home", { replace: true });
+    } else {
+      return navigate("/auth/login");
+    }
+  }, [isTokenLoading, isAuthenticated, isAdmin, navigate]);
+
+  return (
+    <>
+      <Outlet />
+    </>
+  );
+};
+
+export default Auth;
