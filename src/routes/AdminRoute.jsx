d8ec4392f19a0f5b91da77fd8745841a407496ac
Francisco Hernandez <franciscohv95@gmail.com>
2024-10-01 22:59:02 -0300

    update admin route

--- src/routes/AdminRoute.jsx
+++ src/routes/AdminRoute.jsx
@@ -1,26 +1,25 @@
-import { Outlet } from "react-router-dom"
-import { useAuth } from "../hooks/useAuth"
-import NotAllow from "../components/errors/NotAllow"
-import { PrivatePostsProvider } from "../context/privatePosts"
-import { PrivatePostProvider } from "../context/privatePost"
+import { useAuth } from "../hooks/useAuth";
+import NotAllow from "../components/errors/NotAllow";
+import { PrivatePostsProvider } from "../context/privatePosts";
+import { PrivatePostProvider } from "../context/privatePost";
+import AdminLayout from "../pages/admin/AdminLayout";
 
 const AdminRoute = () => {
+  const { isAdmin } = useAuth();
 
-    const {isAdmin} = useAuth()
+  if (!isAdmin()) {
+    return <NotAllow />;
+  }
 
-    if(!isAdmin()) {
-        return <NotAllow/>
-    }
+  return (
+    <>
+      <PrivatePostsProvider>
+        <PrivatePostProvider>
+          <AdminLayout />
+        </PrivatePostProvider>
+      </PrivatePostsProvider>
+    </>
+  );
+};
 
-    return(
-        <>
-            <PrivatePostsProvider>
-                <PrivatePostProvider>
-                    <Outlet/>
-                </PrivatePostProvider>
-            </PrivatePostsProvider>
-        </>
-    )
-}
-
-export default AdminRoute
+export default AdminRoute;
