e886eac0c2f1e20641dcde63e251999c3791747e
Francisco Hernandez <franciscohv95@gmail.com>
2024-10-01 22:58:44 -0300

    update hook for private posts

--- src/hooks/usePrivatePosts.js
+++ src/hooks/usePrivatePosts.js
@@ -1,28 +1,41 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { PrivatePostsContext } from "../context/privatePosts";
 import axios from "axios";
 
 const usePrivatePosts = () => {
   const { data, setData } = useContext(PrivatePostsContext);
 
-  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [notif, setNotif] = useState(null);
 
-  const getPosts = async () => {
+  const getPosts = async ({ page = 1 } = {}) => {
+    setLoading(true);
     try {
       const response = await axios.get(
         `${import.meta.env.VITE_BASE_URL}${import.meta.env.VITE_PRIVATE_URL}${import.meta.env.VITE_POST_URL}`,
+        {
+          params: {
+            page,
+          },
+        },
       );
       setData({
-        posts: response.data.rows,
-        totalPosts: response.data.totalPosts,
-        currentPage: response.data.currentPage,
-        totalPages: response.data.totalPages,
+        posts: response.data.rows || [],
+        totalPosts: response.data.totalPosts || 0,
+        currentPage: response.data.currentPage || 1,
+        totalPages: response.data.totalPages || 1,
       });
     } catch (err) {
       console.log(err);
-      setError(err);
+      setNotif(err);
+    } finally {
+      setLoading(false);
     }
   };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
 
   const updatePublishedPost = (id) => {
     const updateList = data.posts.map((post) =>
@@ -31,7 +44,7 @@ const usePrivatePosts = () => {
     setData({ ...data, posts: updateList });
   };
 
-  return { data, getPosts, updatePublishedPost, error };
+  return { data, loading, notif, getPosts, updatePublishedPost };
 };
 
 export default usePrivatePosts;
